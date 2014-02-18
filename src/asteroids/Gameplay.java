c5a4270eed16f12d311bf47c5520f4f6a14dbffd
abermar <abermar91@gmail.com>
2014-02-18 04:57:31 +0200

    User Story 1
    Here is the implementation of user story 1, i made the changes in
    Gameplay class

--- src/asteroids/Gameplay.java
+++ src/asteroids/Gameplay.java
@@ -7,33 +7,40 @@ package asteroids;
 /**
  *
  * @author Eduardo
+ * @author Alejandro
  */
-
 import processing.core.*;
+import javax.swing.*;
 
 public class Gameplay extends PApplet {
-    
+
     //Game Variables
     Spaceship ship;
     int windowWidth = 900;
     int windowHeight = 700;
-    
+    String op1;
+
     //SETUP SECTION
-    public void setup(){
+    public void setup() {
         size(windowWidth, windowHeight);
         fill(255, 204);
         noStroke();
         ship = new Spaceship();
-       
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String op1s = JOptionPane.showInputDialog(frame, "Introduce your name", "");
+        if (op1s != null) {
+            op1 = op1s;
+            JOptionPane.showMessageDialog(frame, "Thank you so much. Enjoy the game");
+        }
     }
-    
+
     //DRAW SECTION
-    public void draw(){
+    public void draw() {
         background(0);
         ship.display(this);
-        
     }
-    
 }
-
- 
